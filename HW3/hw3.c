4fced6faf8baba7ab66917ff67523e05cd43a6fc
Ram Viswanathan <Ram@Rams-MacBook-Pro.local>
2015-10-28 15:31:48 -0700

    Increased buffer size

--- HW3/hw3.c
+++ HW3/hw3.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 #include <string.h>
 
-#define MAX_STRING_LENGTH 256
+#define MAX_STRING_LENGTH 512
 typedef struct Car_ {
  char make[MAX_STRING_LENGTH];
  char model[MAX_STRING_LENGTH];
